10849c4c088f310b669da42b96d3ddffd4453c39
Jakha <jahongir.sh.mannonov@gmail.com>
2023-03-12 12:45:46 +0500

    full screen mode added

--- Interstitial/src/main/java/com/behad/Interstitial/ui/dialog/BehadInterstitialDialog.kt
+++ Interstitial/src/main/java/com/behad/Interstitial/ui/dialog/BehadInterstitialDialog.kt
@@ -2,11 +2,21 @@ package com.behad.Interstitial.ui.dialog
 
 import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import com.behad.Interstitial.R
 
 class BehadInterstitialDialog(context: Context) : Dialog(context) {
+    override fun onStart() {
+        super.onStart()
+        this.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        this.window?.setLayout(-1, -1)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_behad_interstital_dialog)
         setCanceledOnTouchOutside(false)
         setCancelable(false)
     }
