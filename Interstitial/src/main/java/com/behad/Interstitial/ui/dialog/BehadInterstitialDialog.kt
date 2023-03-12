feb63756fa283fd4acafdb65d5e6d9de2b93f87f
Jakha <jahongir.sh.mannonov@gmail.com>
2023-03-12 12:35:32 +0500

    Dialog added

---
+++ Interstitial/src/main/java/com/behad/Interstitial/ui/dialog/BehadInterstitialDialog.kt
@@ -0,0 +1,11 @@
+package com.behad.Interstitial.ui.dialog
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+
+class BehadInterstitialDialog(context: Context) : Dialog(context) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
