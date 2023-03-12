c02f9d26bd83e428d1c685d084e03fa55bfd6d11
Jakha <jahongir.sh.mannonov@gmail.com>
2023-03-12 16:32:25 +0500

    Networking service added

---
+++ Interstitial/src/main/java/com/behad/Interstitial/ui/model/response/BehadInterstitial.kt
@@ -0,0 +1,9 @@
+package com.behad.Interstitial.ui.model.response
+
+import kotlinx.coroutines.Deferred
+import okhttp3.ResponseBody
+import retrofit2.Response
+
+typealias BehadInterstitialResponse = Deferred<Response<ResponseBody?>>
+
+class BehadInterstitial
---
+++ Interstitial/src/main/java/com/behad/Interstitial/ui/service/BehadInterstitialService.kt
@@ -0,0 +1,14 @@
+package com.behad.Interstitial.ui.service
+
+import com.behad.Interstitial.ui.model.response.BehadInterstitialResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface BehadInterstitialService {
+
+    @GET("https://ads.behad.uz/api/v1//filterAd")
+    fun getAd(
+        @Query("deviceId") deviceId: String,
+        @Query("adId") adId: String,
+    ): BehadInterstitialResponse
+}
